3990fc4d149dab898b2ba8c4c8af1841924e6946
Vadim Silagin <silagin0205@gmail.com>
2025-02-13 14:10:29 +0500

    добавление стилей

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,38 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider__line');
+const images = document.querySelectorAll('.slider__wrapper .slider__line img');
+const sliderLine = document.querySelector('.slider__wrapper .slider__line');
+let count = 0;
+let width;
 
-document.querySelector('slider__next').addEventListener('click', function(){
-    offset += 175;
-    sliderLine.styleless.left = -offset + 'px';
+function init() {
+  console.log('resize');
+  width = document.querySelector('.slider__wrapper').offsetWidth;
+  sliderLine.style.width = width * images.length + 'px';
+  images.forEach(item => {
+      item.style.width = width + 'px';
+      item.style.height = 'auto';
   });
+  rollSlider();
+}
+
+window.addEventListener('resize', init);
+init();
+
+document.querySelector('.slider__prev').addEventListener('click', function() {
+  count--;
+  if (count < 0) {
+      count = images.length - 1;
+  }
+  rollSlider();
+});
+
+document.querySelector('.slider__next').addEventListener('click', function() {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+  sliderLine.style.transform = 'translate(-' + count * width +'px)';
+}
